3ae0572444f0c70e5973f44d543dbc4945ed7fc5
Genisenko <geniscattkd@gmail.com>
2022-08-11 08:36:26 +0200

    vue setup finished

---
+++ lab-vue-setup/src/components/ToDoItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <label for="todo-item">My To-Do Item</label>
+    <input type="checkbox" id="todo-item">
+
+
+    <h1>Message: {{ msg }}</h1>
+
+
+  </div>
+  
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            msg: "This is a text interpolation",
+        }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
